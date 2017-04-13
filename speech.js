9164dfcf69cadf3711164bef55364a1254ddea46
ilook <look.wangluke@gmail.com>
2017-04-13 16:50:15 +0800

    Update speech.js

--- speech.js
+++ speech.js
@@ -37,8 +37,8 @@ function exp() {
   //for (var i =0; i<1000000;i++){};
   //recognition.abort();	
   //eval("recognition.stop(); alert(1);");
-  //setTimeout('recognition.stop();', 9500+index*10);
-  console.log("dick " + (9500+index*10));
+  setTimeout('recognition.abort();', 5000+index*10);
+  console.log("dick " + (5000+index*10));
   index = index+1;
   //alert(1);
 }
