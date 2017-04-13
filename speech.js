c8c382136860a2fbf9a2a58e11a498d22d8d65b2
ilook <look.wangluke@gmail.com>
2017-04-13 10:21:17 +0800

    Update speech.js

--- speech.js
+++ speech.js
@@ -35,7 +35,7 @@ document.body.onclick = function() {
   //for (var i =0; i<1000000;i++){};
   //recognition.abort();	
   //eval("recognition.stop(); alert(1);");
-  setTimeout('recognition.abort();', 3000);
+  setTimeout('recognition.start();', 6000);
 	
   alert(1);
   console.log('Ready to receive a color command.');
