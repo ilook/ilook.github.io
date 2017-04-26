3a3094d83f3efe0aca904fc400ccaf43d66937ab
ilook <look.wangluke@gmail.com>
2017-04-26 23:01:07 +0800

    Update speech.js

--- speech.js
+++ speech.js
@@ -63,7 +63,8 @@ navigator.bluetooth.requestDevice(options).then(function(device) {
   console.log('Name: ' + device.name);
   // Do something with the device.
 }) 
-  
+console.log('navigator.bluetooth.requestDevice');
+ exp(); 
 }
 
 function exp() {
