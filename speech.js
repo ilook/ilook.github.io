6a7dc5d9cc597d3b6b5733c362fd24731904761a
ilook <look.wangluke@gmail.com>
2017-05-07 01:39:01 +0800

    Update speech.js

--- speech.js
+++ speech.js
@@ -102,9 +102,9 @@ function init()
 window.setTimeout(exp, 2000);
 
 recognition.onerror  = function(event) {
-  for(var i =0; i<2500;i++);
+  //for(var i =0; i<2500;i++);
   try{recognition.abort();}catch(e){}
-  window.setTimeout("location.reload()", 2000);
+  //window.setTimeout("location.reload()", 2000);
 } 
 
 recognition.onresult = function(event) {
